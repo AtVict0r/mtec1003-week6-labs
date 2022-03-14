17731d2a689764313e118e95b5da4ad2237e4a6c
Oluwasegun Atolagbe <oluwasegun.atolagbe@mail.citytech.cuny.edu>
2022-03-14 16:51:25 -0400

    Added JS files miles and shout, Added PY file bmi

---
+++ py/bmi.py
@@ -0,0 +1,10 @@
+#import sys # prevent error from weird characters
+
+weight = float(input("Enter your weight (in pounds): "))
+print("You entered " + str(weight) + " pounds")
+
+height = float(input("Enter your height (in inches): "))
+print("You entered " + str(height) + " inches")
+
+bmi = (703 * weight) / (height ** 2)
+print("Your body mass index (BMI) is " + str(bmi))
--- py/saytwice.py
+++ py/saytwice.py
@@ -1,3 +1,3 @@
 word = str(input("Give me a word to say twice: "))
-twice = word + ' ' + word ##(word + ' ') * 2
+twice = word + ' ' + word # is the same as (word + ' ') * 2
 print(twice)
