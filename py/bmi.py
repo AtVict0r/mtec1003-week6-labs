854fd5a9236ccba22f8d216af2e45fd9abd76f97
Oluwasegun Atolagbe <oluwasegun.atolagbe@mail.citytech.cuny.edu>
2022-03-28 19:58:33 -0400

    Updated bmi.py, removed import

--- py/bmi.py
+++ py/bmi.py
@@ -1,4 +1,4 @@
-#import sys # prevent error from weird characters
+print("BMI: Body Mass Index Calculator")
 
 weight = float(input("Enter your weight (in pounds): "))
 print("You entered " + str(weight) + " pounds")
